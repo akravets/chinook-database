22ebd191fe7074b8c72fa0e21b37c67782c0b9d7
akravets <kravets.dev@gmail.com>
2020-03-11 11:22:30 -0400

    Add grouping of Supervisors by their Employees

---
+++ scripts/NumberOfEmployeesPerSupervisor.sql
@@ -0,0 +1,15 @@
+/*
+  Groups supervisors by their number of employees
+*/
+SELECT 
+  a.EmployeeId as SupervisorId, 
+  a.FirstName as SupervisorName, 
+  count(b.EmployeeId) as number_of_employees
+
+  FROM 
+    Employee a, Employee b
+  WHERE 
+    a.EmployeeId=b.ReportsTo
+  GROUP BY 
+    SupervisorName
+    ORDER BY SupervisorId
